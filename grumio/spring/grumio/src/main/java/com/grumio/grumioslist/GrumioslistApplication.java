a0e969bd4e429339ae7992cf74b474d8bdc507da
Joey736n <josephbinette67@gmail.com>
2024-02-16 19:54:18 -0500

    spring initial setup

---
+++ grumio/spring/grumio/src/main/java/com/grumio/grumioslist/GrumioslistApplication.java
@@ -0,0 +1,13 @@
+package com.grumio.grumioslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrumioslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrumioslistApplication.class, args);
+	}
+
+}
---
+++ grumio/spring/grumio/src/test/java/com/grumio/grumioslist/GrumioslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grumio.grumioslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrumioslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
